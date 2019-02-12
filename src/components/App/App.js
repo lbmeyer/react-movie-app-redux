f98ffc62a02c7f3bf9357d8ea7a56f0fc5a8c1e3
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2019-02-12 15:54:01 -0500

    Added HeroImage and SearchBar components in Home.js. Added JSX in render and doSearch function in SearchBar.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,8 +9,6 @@ const App = () => {
   return (
     <div>
       <Header />
-      <HeroImage />
-      <SearchBar />
       <Home />
     </div>
   )
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE
+} from '../../config';
 import HeroImage from '../elements/HeroImage/HeroImage';
 import SearchBar from '../elements/SearchBar/SearchBar';
 import FourColGrid from '../elements/FourColGrid/FourColGrid';
@@ -9,19 +16,96 @@ import './Home.css';
 
 class Home extends Component {
   state = {
+    movies: [],
+    heroImage: null,
+    loading: false,
+    currentPage: 0,
+    totalPages: 0,
+    searchTerm: ''
+  };
 
+  componentDidMount() {
+    this.setState({ loading: true });
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
+  }
+
+  fetchItems = endpoint => {
+    fetch(endpoint)
+      .then(endpoint)
+      .then(res => res.json())
+      .then(result => {
+        // console.log(result);
+        this.setState({
+          movies: [...this.state.movies, ...result.results],
+          heroImage: this.state.heroImage || result.results[0], // if this.state.heroImage is null, use result.result[0]
+          loading: false,
+          currentPage: result.page,
+          totalPages: result.total_pages
+        });
+      });
+  };
+
+  loadMoreItems = () => {
+    let endpoint = '';
+    this.setState({ loading: true });
+
+    // if not searching for a movie, api get the next page of movies
+    if (this.setState.searchTerm === '') {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this
+        .state.currentPage + 1}`;
+    } else {
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query${
+        this.state.searchTerm
+      }&page=${this.state.currentPage + 1}`;
+    }
+    this.fetchItems(endpoint);
+  };
+
+  searchItems = searchTerm => {
+    console.log(searchTerm)
+    let endpoint = '';
+    this.setState({
+      movies: [],
+      loading: true,
+      searchTerm
+    });
+
+    if (!searchTerm) {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    } else {
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+    }
+    this.fetchItems(endpoint);
   }
 
   render() {
+    const {
+      movies,
+      heroImage,
+      loading,
+      currentPage,
+      totalPages,
+      searchTerm
+    } = this.state;
+
     return (
       <div className="rmdb-home">
-        <HeroImage />
-        <SearchBar />
+        {this.state.heroImage ? (
+          <div>
+            <HeroImage
+              image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+              title={heroImage.original_title}
+              text={heroImage.overview}
+            /> 
+            <SearchBar callback={this.searchItems} />
+          </div>
+        ) : null }
         <FourColGrid />
         <Spinner />
         <LoadMoreBtn />
       </div>
-    )
+    );
   }
 }
 
